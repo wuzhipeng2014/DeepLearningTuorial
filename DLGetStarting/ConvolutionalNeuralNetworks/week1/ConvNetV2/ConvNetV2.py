ae20850337b3579655870c4f829360ce3dcb8e6b
zhipeng.wu <zhipeng.wu@qunar.com>
2018-01-30 11:19:41 +0800

    MOD:卷积网络的Python实现

--- DLGetStarting/ConvolutionalNeuralNetworks/week1/ConvNetV2/ConvNetV2.py
+++ DLGetStarting/ConvolutionalNeuralNetworks/week1/ConvNetV2/ConvNetV2.py
@@ -15,17 +15,62 @@ np.random.seed(1)
 np.random.seed(1)
 x = np.random.randn(4, 3, 3, 2)
 x_pad = zero_pad(x, 2)
-print ("x.shape =", x.shape)
-print ("x_pad.shape =", x_pad.shape)
-print ("x[1,1] =", x[1,1])
-print ("x_pad[1,1] =", x_pad[1,1])
+print("x.shape =", x.shape)
+print("x_pad.shape =", x_pad.shape)
+print("x[1,1] =", x[1, 1])
+print("x_pad[1,1] =", x_pad[1, 1])
 
 fig, axarr = plt.subplots(1, 2)
 axarr[0].set_title('x')
-axarr[0].imshow(x[1,:,:,1])
+axarr[0].imshow(x[1, :, :, 1])
 axarr[1].set_title('x_pad')
-axarr[1].imshow(x_pad[1,:,:,1])
+axarr[1].imshow(x_pad[1, :, :, 1])
 
 plt.show()
 
-##
+## 模拟一步卷积的实现过程
+np.random.seed(1)
+a_slice_prev = np.random.randn(4, 4, 3)
+W = np.random.randn(4, 4, 3)
+b = np.random.randn(1, 1, 1)
+
+Z = conv_single_step(a_slice_prev, W, b)
+print("Z =", Z)
+
+## 卷积实现
+
+np.random.seed(1)
+A_prev = np.random.randn(10, 4, 4, 3)
+W = np.random.randn(2, 2, 3, 8)
+b = np.random.randn(1, 1, 1, 8)
+hparameters = {"pad": 2,
+               "stride": 2}
+
+Z, cache_conv = conv_forward(A_prev, W, b, hparameters)
+print("Z's mean =", np.mean(Z))
+print("Z[3,2,1] =", Z[3, 2, 1])
+print("cache_conv[0][1][2][3] =", cache_conv[0][1][2][3])
+
+## pooling layer
+np.random.seed(1)
+A_prev = np.random.randn(2, 4, 4, 3)
+hparameters = {"stride": 2, "f": 3}
+
+A, cache = pool_forward(A_prev, hparameters)
+print("mode = max")
+print("A =", A)
+print()
+A, cache = pool_forward(A_prev, hparameters, mode="average")
+print("mode = average")
+print("A =", A)
+
+##todo 卷积层的反向传播(难点)
+
+
+##todo 池化层的反向传播(难点)
+
+
+## 模型整合
+
+
+## 模型测试
