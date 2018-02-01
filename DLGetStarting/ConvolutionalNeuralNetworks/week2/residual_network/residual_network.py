bab7ef17e46d4db83c5f4abca35889defb1b5cf0
zhipeng.wu <zhipeng.wu@qunar.com>
2018-02-01 17:10:05 +0800

    MOD:resnet 50测试

--- DLGetStarting/ConvolutionalNeuralNetworks/week2/residual_network/residual_network.py
+++ DLGetStarting/ConvolutionalNeuralNetworks/week2/residual_network/residual_network.py
@@ -47,3 +47,60 @@ with tf.Session() as test:
     test.run(tf.global_variables_initializer())
     out = test.run([A], feed_dict={A_prev: X, K.learning_phase(): 0})
     print("out = " + str(out[0][1][1][0]))
+
+## resnet 50测试
+model = ResNet50(input_shape = (64, 64, 3), classes = 6)
+model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
+
+X_train_orig, Y_train_orig, X_test_orig, Y_test_orig, classes = load_dataset()
+
+# Normalize image vectors
+X_train = X_train_orig/255.
+X_test = X_test_orig/255.
+
+# Convert training and test labels to one hot matrices
+Y_train = convert_to_one_hot(Y_train_orig, 6).T
+Y_test = convert_to_one_hot(Y_test_orig, 6).T
+
+print ("number of training examples = " + str(X_train.shape[0]))
+print ("number of test examples = " + str(X_test.shape[0]))
+print ("X_train shape: " + str(X_train.shape))
+print ("Y_train shape: " + str(Y_train.shape))
+print ("X_test shape: " + str(X_test.shape))
+print ("Y_test shape: " + str(Y_test.shape))
+
+## todo (训练模型)
+model.fit(X_train, Y_train, epochs = 2, batch_size = 32)
+
+preds = model.evaluate(X_test, Y_test)
+print ("Loss = " + str(preds[0]))
+print ("Test Accuracy = " + str(preds[1]))
+
+model.save("model/resnet.model")
+
+
+## 加载训练好的resnet50
+model = load_model('ResNet50.h5')
+
+preds = model.evaluate(X_test, Y_test)
+print ("Loss = " + str(preds[0]))
+print ("Test Accuracy = " + str(preds[1]))
+
+## 在自己的训练集上测试
+img_path = 'images/2.jpg'
+img = image.load_img(img_path, target_size=(64, 64))
+x = image.img_to_array(img)
+x = np.expand_dims(x, axis=0)
+x = preprocess_input(x)
+print('Input image shape:', x.shape)
+my_image = scipy.misc.imread(img_path)
+imshow(my_image)
+print("class prediction vector [p(0), p(1), p(2), p(3), p(4), p(5)] = ")
+print(model.predict(x))
+
+model.summary()
+
+## 图像展示resnet50 的网络结构
+
+plot_model(model, to_file='model.png')
+SVG(model_to_dot(model).create(prog='dot', format='svg'))
